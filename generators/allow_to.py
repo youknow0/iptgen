85d264e7221d05c63c1dc96db9db7161c121d480
Nico Boehr <youknow0@users.noreply.github.com>
2015-06-19 20:49:29 +0200

    allow_to: take destination ip adresses from host section

--- generators/allow_to.py
+++ generators/allow_to.py
@@ -19,12 +19,13 @@ class AllowToGenerator(object):
 
         # generate one rule per addr of the host
         rule_out = ""
-        for addr in addr_generator.addrs[rule.host]:
-            myrule = rule_str + ("--dport %s -s %s -d %s" 
-                                    % (rule.port, addr, rule.dest))
-            myrule = call_hook("rule_allow_to", 
-                                 myrule, rule)
-            rule_out += myrule + linesep
+        for src_addr in addr_generator.addrs[rule.host]:
+            for dest_addr in addr_generator.addrs[rule.dest]:
+                myrule = rule_str + ("--dport %s -s %s -d %s" 
+                                        % (rule.port, src_addr, dest_addr))
+                myrule = call_hook("rule_allow_to", 
+                                     myrule, rule)
+                rule_out += myrule + linesep
 
 
         return rule_out
